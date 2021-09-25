bfe0ba9df2deed5830774f519562314c9860d709
Yu-Muro <6u38e7c16rnhg5e37a0z@gmail.com>
2021-09-26 05:31:34 +0900

    「追加」発言時に変更

--- main.py
+++ main.py
@@ -49,9 +49,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
+    if "追加" in event.message.text:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text = "何を追加しますか？"))
 
 # ポート番号の設定
 if __name__ == "__main__":
