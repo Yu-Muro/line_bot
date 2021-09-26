15b97f0981cda129e0fc9132bbcb6e741793a043
Yu-Muro <6u38e7c16rnhg5e37a0z@gmail.com>
2021-09-27 04:43:29 +0900

    モジュールの編集

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from flask-SQLAlchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 
